1d61ef7a52b87f6e35918a8dd45c5178941cf928
prashi mishra <108418734+prashi1011@users.noreply.github.com>
2022-07-29 13:03:17 +0530

    Add files via upload

---
+++ Cube_of_numbers.java
@@ -0,0 +1,21 @@
+package Prashijavapractice.java;
+
+import java.util.Scanner;
+public class Cube_of_numbers {
+
+        public static void main(String[] args)
+
+        {
+            int i,n;
+
+            System.out.print("Input number of terms : ");
+            Scanner in = new Scanner(System.in);
+            n = in.nextInt();
+
+            for(i=1;i<=n;i++)
+            {
+                System.out.println("Number is : " +i+" and cube of " +i+" is : "+(i*i*i));
+            }
+        }
+    }
+
