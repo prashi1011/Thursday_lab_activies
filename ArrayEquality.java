f60aac58b3f35906950fb69287608576a83e58fe
prashi mishra <108418734+prashi1011@users.noreply.github.com>
2022-08-04 17:50:48 +0530

    Add files via upload

---
+++ ArrayEquality.java
@@ -0,0 +1,19 @@
+package Prashijavapractice.java;
+
+//a program to check array equality
+
+
+import java. util. Arrays;
+
+public class ArrayEquality
+{
+
+
+    public static void main(String args[])
+    {
+        int[]arr1=new int[]{20, 5, -95, 43, 17};
+        int[]arr2=new int[]{20, 5, -95, 43, 17};
+        System. out. println(Arrays. equals(arr1, arr2));
+    }
+}
+
---
+++ RotateArray.java
@@ -0,0 +1,25 @@
+package Prashijavapractice.java;
+
+//a program to rotate array
+
+
+public class RotateArray {
+
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int n = arr.length;
+            int last, i;
+            System.out.print("The original array is: ");
+            for (i = 0; i < n; ++i)
+                System.out.print(arr[i] + " ");
+            last = arr[n-1];
+            for (i = n-1; i > 0; i--)
+                arr[i] = arr[i-1];
+            arr[0] = last;
+            System.out.print("\nThe rotated Array is: ");
+            for (i = 0; i < n; ++i)
+                System.out.print(arr[i] + " ");
+        }
+
+
+}
