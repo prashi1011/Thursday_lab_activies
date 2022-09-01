14999a3ec4e10d2636b91bda7f1834c11a2ff65c
prashi mishra <108418734+prashi1011@users.noreply.github.com>
2022-09-01 19:40:16 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Swap {  
+	int a, b;  
+	public void swapNum(SwapNumDemo stn) {  
+		int temp;  
+		temp = stn.a;  
+		stn.a = stn.b;  
+		stn.b = temp;  
+	}  
+	public static void main(String args[]) {  
+		Swap sn = new Swap();  
+		Scanner sc = new Scanner(System.in);               
+		System.out.print("Enter the first number: ");  
+		sn.a = sc.nextInt();  
+		System.out.print("Enter the second number: ");  
+		sn.b = sc.nextInt();  
+		sn.swapNum(sn);  
+		System.out.println("After swapping: a= " + sn.a + ", b= " + sn.b);  
+	}  
+}  
